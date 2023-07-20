0f245adaa888565cb33e9a9114bce8362791aaee
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-07-20 10:19:46 -0300

    Doing some Exercises
    Studying a book that gives a bunch of exercises, doing some researches to learn more about DOM in JS.
    I should improve a little on the front-end of "modelo2.html" but it's just studying sessions.

--- js/script.js
+++ js/script.js
@@ -28,10 +28,71 @@ document.getElementById("btn-calcular").onclick = function(){
 document.querySelector("#btn-calcular").addEventListener("click", function(){
 	let valorA = document.querySelector("#valor-a").value;
 	let valorB = document.querySelector("#valor-b").value;
-	if (document.querySelector("#sub").checked == true) {
+
+	if (document.querySelector("#sub").checked == true)
+	{
 	alert(parseInt(valorA) - parseInt(valorB));
-	}else{
-	if(valorA.length > 0 && valorB.length > 0){
-	alert(parseInt(valorA) + parseInt(valorB));}
-	else{alert("Por favor, insira um número nas posições!")}}
+	}
+	else
+	{
+		if (document.querySelector("#exb").checked == true)
+		{
+			let areaC = (3.14 * (valorB * valorB)) + (2 * 3.14 * valorA * valorB);
+			litro = areaC / 3;
+			quant = Math.round(litro / 5)
+			custo = quant * 50
+			alert("Para pintar o cilindro com as informações recebidas, você irá precisar de um total de " + parseInt(quant) + " latas, totalizando o valor de R$ " + parseInt(custo));
+		}
+		else
+		{
+			if(document.querySelector("#ex1").checked == true)
+			{
+				let valorC = document.querySelector("#valor-c").value;
+
+				let x = ((-valorB + (Math.sqrt((valorB * valorB) - 4 * valorA * valorC))) / (2 * valorA));
+				let xNeg = ((-valorB - (Math.sqrt((valorB * valorB) - 4 * valorA * valorC))) / (2 * valorA));
+
+				alert("X pode ser: " + x + " ou " + xNeg);
+			}
+			else
+			{
+				if(document.querySelector("#ex3").checked == true)
+				{
+					let volume = (4 / 3) * (3.14 * (Math.pow(valorA, 3)));
+					alert("O Volume da esfera é: " + volume);
+				}
+				else
+				{
+					if(valorA.length > 0 && valorB.length > 0)
+					{
+					alert(parseInt(valorA) + parseInt(valorB));
+					}
+					else
+					{
+						alert("Por favor, insira um número nas posições!")
+					}
+				}
+			}
+		}
+	}
 });
+
+document.querySelector("#ex1").addEventListener("change", function(){
+	var form = document.querySelector('#form');
+
+	if(document.querySelector("#ex1").checked == true)
+	{
+		var novoItem = document.createElement("input");
+		novoItem.setAttribute('type', 'number');
+		novoItem.setAttribute('id', 'valor-c');
+		novoItem.setAttribute('placeholder', 'valor 3');
+		console.log(novoItem)
+		form.appendChild(novoItem);
+	}
+	else
+	{
+		var delItem = document.querySelector("#valor-c");
+		delItem.remove();
+	}
+
+})
